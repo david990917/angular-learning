2f0d5c268177693022a7255004cb0d1285bc5e63
Hanwen <hanwen.liu@tum.de>
2022-12-13 15:33:17 +0100

    update to support router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, AfterViewInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { ShepherdService } from 'angular-shepherd';
 
 @Component({
@@ -7,14 +8,147 @@ import { ShepherdService } from 'angular-shepherd';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements AfterViewInit {
-  constructor(private shepherdService: ShepherdService) { }
+  constructor(
+    private shepherdService: ShepherdService,
+    private router: Router) { }
   ngAfterViewInit() {
     this.shepherdService.defaultStepOptions = tour.defaultStepOptions;
     this.shepherdService.modal = true;
     this.shepherdService.confirmCancel = false;
-    this.shepherdService.addSteps(steps);
+    this.shepherdService.addSteps(this.steps);
     this.shepherdService.start();
   }
+
+  steps = [
+    {
+      id: "example-step1",
+      title: "Step 1",
+      text: "Welcome to Hanwen's projects",
+      attachTo: {
+        element: ".example-step1",
+        on: "bottom"
+      },
+      buttons: [
+        // {
+        //   classes: "cancel-button",
+        //   text: "Exit",
+        //   type: "cancel"
+        // },
+        {
+          classes: "shepherd-button-primary",
+          text: "Back",
+          type: "back"
+        },
+        {
+          classes: "shepherd-button-primary",
+          text: "Next",
+          type: "next"
+        }
+      ],
+      classes: '',
+      highlightClass: "highlight",
+    }, {
+      id: "example-step2",
+      title: "Step 2",
+      text: "Hanwen's demo2",
+      attachTo: {
+        element: ".example-step2",
+        on: "bottom"
+      },
+      buttons: [
+        {
+          classes: "shepherd-button-primary",
+          text: "Back",
+          type: "back"
+        },
+        {
+          classes: "shepherd-button-primary",
+          text: "Next",
+          type: "next"
+        }
+      ],
+      classes: '',
+      highlightClass: "highlight",
+
+    }, {
+      id: "example-step3",
+      title: "Step 3",
+      text: "Hanwen's demo3",
+      attachTo: {
+        element: ".example-step3",
+        on: "bottom"
+      },
+      buttons: [
+        {
+          classes: "shepherd-button-primary",
+          text: "Back",
+          type: "back"
+        },
+        {
+          classes: "shepherd-button-primary",
+          text: "Next",
+          type: "next"
+        }
+      ],
+      classes: '',
+      highlightClass: "highlight",
+
+    }, {
+      id: "example-step4",
+      title: "Step 4",
+      text: "Hanwen's demo4",
+      attachTo: {
+        element: ".example-step4",
+        on: "bottom"
+      },
+      buttons: [
+        {
+          classes: "shepherd-button-primary",
+          text: "Back",
+          type: "back"
+        },
+        {
+          classes: "shepherd-button-primary",
+          text: "Next",
+          type: "next"
+        },
+        {
+          classes: "shepherd-button-primary",
+          text: "Switch Page",
+          action: () => {
+            this.router.navigateByUrl('/cart')
+            this.shepherdService.next()
+          }
+        }
+      ],
+      classes: '',
+      highlightClass: "highlight",
+
+    }, {
+      id: "example-step5",
+      title: "Step 5",
+      text: "Hanwen's demo5",
+      attachTo: {
+        element: ".example-step5",
+        on: "bottom"
+      },
+      buttons: [
+        {
+          classes: "shepherd-button-primary",
+          text: "Back",
+          type: "back"
+        },
+        {
+          classes: "shepherd-button-primary",
+          text: "Next",
+          type: "next"
+        }
+      ],
+      classes: '',
+      highlightClass: "highlight",
+
+    }
+  ];
 
 }
 
@@ -47,125 +181,3 @@ const tour = {
   },
 };
 
-const steps = [
-  {
-    id: "example-step1",
-    title: "Step 1",
-    text: "Welcome to Hanwen's projects",
-    attachTo: {
-      element: ".example-step1",
-      on: "bottom"
-    },
-    buttons: [
-      // {
-      //   classes: "cancel-button",
-      //   text: "Exit",
-      //   type: "cancel"
-      // },
-      {
-        classes: "shepherd-button-primary",
-        text: "Back",
-        type: "back"
-      },
-      {
-        classes: "shepherd-button-primary",
-        text: "Next",
-        type: "next"
-      }
-    ],
-    classes: '',
-    highlightClass: "highlight",
-  }, {
-    id: "example-step2",
-    title: "Step 2",
-    text: "Hanwen's demo2",
-    attachTo: {
-      element: ".example-step2",
-      on: "bottom"
-    },
-    buttons: [
-      {
-        classes: "shepherd-button-primary",
-        text: "Back",
-        type: "back"
-      },
-      {
-        classes: "shepherd-button-primary",
-        text: "Next",
-        type: "next"
-      }
-    ],
-    classes: '',
-    highlightClass: "highlight",
-
-  }, {
-    id: "example-step3",
-    title: "Step 3",
-    text: "Hanwen's demo3",
-    attachTo: {
-      element: ".example-step3",
-      on: "bottom"
-    },
-    buttons: [
-      {
-        classes: "shepherd-button-primary",
-        text: "Back",
-        type: "back"
-      },
-      {
-        classes: "shepherd-button-primary",
-        text: "Next",
-        type: "next"
-      }
-    ],
-    classes: '',
-    highlightClass: "highlight",
-
-  }, {
-    id: "example-step4",
-    title: "Step 4",
-    text: "Hanwen's demo4",
-    attachTo: {
-      element: ".example-step4",
-      on: "bottom"
-    },
-    buttons: [
-      {
-        classes: "shepherd-button-primary",
-        text: "Back",
-        type: "back"
-      },
-      {
-        classes: "shepherd-button-primary",
-        text: "Next",
-        type: "next"
-      }
-    ],
-    classes: '',
-    highlightClass: "highlight",
-
-  }, {
-    id: "example-step5",
-    title: "Step 5",
-    text: "Hanwen's demo5",
-    attachTo: {
-      element: ".example-step5",
-      on: "bottom"
-    },
-    buttons: [
-      {
-        classes: "shepherd-button-primary",
-        text: "Back",
-        type: "back"
-      },
-      {
-        classes: "shepherd-button-primary",
-        text: "Next",
-        type: "next"
-      }
-    ],
-    classes: '',
-    highlightClass: "highlight",
-
-  }
-];
